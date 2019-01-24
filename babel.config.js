0d9dcaa35154ae0a3b47d5f101061e1e355fd6ea
tzweb <821189067@qq.com>
2019-01-24 11:12:11 +0800

    修复项目在ie11、ie10 出现语法错误的问题

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    [
+      '@vue/app',
+      {
+        useBuiltIns: 'entry'
+      }
+    ]
   ],
   plugins: [
     [
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill'
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
