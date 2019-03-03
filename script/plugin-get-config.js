a487e31ad18df94bff6721ab8e72810f79f984b8
Zhanglinling <zhanglinling0926@gmail.com>
2019-03-03 17:05:01 +0800

    chore(*): 自动创建插件及对应路由等处理

--- script/plugin-get-config.js
+++ script/plugin-get-config.js
@@ -1,39 +1,64 @@
 const fs = require('fs-extra')
 const path = require('path')
 const chalk = require('chalk')
+const dirTree = require('directory-tree')
+const getViewConfig = require('./lib/view-get-config')
 
-function isDirectory(source) {
+function isPlugin(source) {
   return fs.lstatSync(source).isDirectory()
 }
 
-function isPlugin(source) {
-  return isDirectory(source)
-}
-
 function getPlugins(source) {
-  const srcPath = path.resolve(__dirname, '../src')
   const folders = fs.readdirSync(source)
-  const pluginsConfig = [];
+  const pluginsList = []
 
   folders.forEach((item) => {
     const itemPath = path.join(source, item)
     if (!isPlugin(itemPath)) {
       return
     }
-    const config = {};
+    const config = {}
     config.name = item
-    config.path = `@/plugins/${item}/`;
-    config.package = JSON.parse(fs.readFileSync(path.resolve(itemPath, './package.json'), 'utf8'));
-    pluginsConfig.push(config)
+    config.path = path.resolve(__dirname, `../src/plugins/${item}/`)
+    config.package = JSON.parse(fs.readFileSync(path.resolve(itemPath, './package.json'), 'utf8'))
+    pluginsList.push(config)
   })
 
-  return pluginsConfig
+  return pluginsList
 }
 
-const pluginsPath = path.resolve(__dirname, '../src/plugins')
-const pluginsConfig = getPlugins(pluginsPath)
-const targetDir = path.resolve(__dirname, '../src/plugins/configs.json')
+function iteratTree(treeObj, pluginName) {
+  let result
+  if (treeObj.type === 'directory') {
+    result = {
+      type: 'directory',
+      name: treeObj.name,
+    }
+    result.children = treeObj.children.map(item => iteratTree(item, pluginName))
+      .filter(item => !!item)
+  } else {
+    result = getViewConfig(treeObj, pluginName)
+  }
+  return result
+}
 
-fs.outputFileSync(targetDir, JSON.stringify(pluginsConfig));
 // eslint-disable-next-line
-console.log(chalk.green(`已获取插件配置: path: ${targetDir}`))
+console.log(chalk.green('开始自动构建插件配置...\n'));
+
+const targetDir = path.resolve(__dirname, '../src/plugins/configs.json')
+const pluginsPath = path.resolve(__dirname, '../src/plugins')
+const viewConfig = {}
+const puginList = getPlugins(pluginsPath)
+
+puginList.forEach((item) => {
+  const viewPath = path.resolve(item.path, './views')
+  const tree = dirTree(viewPath, {
+    extensions: /\.vue$/,
+  })
+  viewConfig[item.name] = iteratTree(tree, item.name)
+})
+
+fs.writeFile(targetDir, JSON.stringify(viewConfig, null, 2))
+// fs.outputFileSync(targetDir, JSON.stringify(pluginsConfig));
+// eslint-disable-next-line
+console.log(chalk.green(`插件配置完成: ${targetDir}`))
--- script/plugin-new.js
+++ script/plugin-new.js
@@ -3,6 +3,7 @@ const path = require('path')
 const inquirer = require('inquirer')
 const ejs = require('ejs')
 const chalk = require('chalk')
+const yaml = require('js-yaml')
 
 const questions = []
 
@@ -37,6 +38,13 @@ questions.push({
 
 questions.push({
   type: 'input',
+  name: 'title',
+  message: '请输入插件标题, 7个字内\n',
+  default: '插件标题',
+})
+
+questions.push({
+  type: 'input',
   name: 'version',
   message: '请输入版本号\n',
   default: '1.0.0',
@@ -55,6 +63,13 @@ questions.push({
   message: '请输入作者\n',
   default: '',
 })
+
+// questions.push({
+//   type: 'confirm',
+//   name: 'hasView',
+//   message: '是否包含舞台页面\n',
+//   default: true,
+// })
 
 inquirer.prompt(questions).then((answers) => {
   const result = answers
@@ -75,6 +90,19 @@ inquirer.prompt(questions).then((answers) => {
   }
   fs.mkdirSync(cachePluginPath)
 
+  // 页面基础配置
+  const viewConfig = {
+    title: 'View',
+    path: '/view',
+    inNav: true,
+    name: 'View',
+    permission: [],
+    needLogin: true,
+    order: '',
+    icon: 'iconfont icon-demo',
+  }
+
+  // 基础组成
   const structure = [{
     path: 'README.md',
   }, {
@@ -87,16 +115,65 @@ inquirer.prompt(questions).then((answers) => {
     path: 'generator.js',
   }, {
     path: 'views/TestView.vue',
+    config: Object.assign({}, viewConfig, {
+      title: 'TestView',
+      path: `/${config.camelCaseName}/testview`,
+      name: `${config.camelCaseName}TestView`,
+    }),
   }, {
     path: 'components/Test.vue',
   }]
 
+  // 主模块组成
+  const stageStructure = [{
+    path: 'views/View1.vue',
+    config: Object.assign({}, viewConfig, {
+      title: 'View1',
+      path: `/${config.camelCaseName}/view1`,
+      name: `${config.camelCaseName}View1`,
+    }),
+  }, {
+    path: 'views/View2/index.vue',
+    config: Object.assign({}, viewConfig, {
+      title: 'View2',
+      path: `/${config.camelCaseName}/view2`,
+      name: `${config.camelCaseName}View2`,
+    }),
+  }, {
+    path: 'views/View2/SubView.vue',
+    config: Object.assign({}, viewConfig, {
+      title: `/${config.camelCaseName}/SubView`,
+      path: '/subview',
+      name: `${config.camelCaseName}SubView`,
+    }),
+  }]
+
+  // 基础内容构建
   structure.forEach((file) => {
     const template = fs.readFileSync(path.resolve(__dirname, `./template/plugin/${file.path}.ejs`), 'utf8')
-    const result = ejs.render(template, config)
+    const fileConfig = { ...config }
+    if (file.config) {
+      fileConfig.configYml = yaml.dump(file.config)
+    }
+    const result = ejs.render(template, fileConfig)
     const targetPath = path.resolve(__dirname, `./.cache/plugin/${file.path}`)
     fs.outputFileSync(targetPath, result)
   })
+
+  // 舞台模块构建
+  if (true) {
+    // if (config.hasView) {
+    stageStructure.forEach((file) => {
+      const template = fs.readFileSync(path.resolve(__dirname, `./template/plugin/${file.path}.ejs`), 'utf8')
+      const fileConfig = { ...config }
+      if (file.config) {
+        fileConfig.configYml = yaml.dump(file.config)
+      }
+      const result = ejs.render(template, fileConfig)
+      const targetPath = path.resolve(__dirname, `./.cache/plugin/${file.path}`)
+      fs.outputFileSync(targetPath, result)
+    })
+  }
 
   return config
 }).then((answers) => {
@@ -105,6 +182,10 @@ inquirer.prompt(questions).then((answers) => {
   const targetPath = path.resolve(__dirname, `../src/plugins/${answers.camelCaseName}`)
   fs.copySync(sourcePath, targetPath)
   console.log(chalk.green(`创建插件${answers.name}: ${targetPath}`))
+  // eslint-disable-next-line
+}).then(() => {
+  // eslint-disable-next-line
+  require('./plugin-get-config.js');
   // eslint-disable-next-line
 }).catch((err) => {
   // eslint-disable-next-line
