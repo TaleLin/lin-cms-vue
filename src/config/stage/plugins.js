4ee294205ec20395451f7d0584f711ffeb1a778c
Zhanglinling <zhanglinling0926@gmail.com>
2019-03-24 21:15:13 +0800

    style: 删除无用的代码

--- src/config/stage/plugins.js
+++ src/config/stage/plugins.js
@@ -1,10 +1,6 @@
 // 本文件是自动生成, 请勿修改
-import Test1 from '@/plugins/Test1/stage-config'
-import Test2 from '@/plugins/Test2/stage-config'
 
 const pluginsConfig = [
-  Test1,
-  Test2,
 ]
 
 export default pluginsConfig
