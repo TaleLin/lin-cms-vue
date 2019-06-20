ff2ddf23a5a08aeacc5d2d59c2ac63cf88b848f7
JohnStream <1021283712@qq.com>
2019-06-20 09:58:15 +0800

    refactor(linCmsUi): 修改目录结构，按照组件类型分类，与Element官方保持一致

--- src/plugins/LinCmsUi/views/basic/button/Button.vue
+++ src/plugins/LinCmsUi/views/basic/button/Button.vue
@@ -264,5 +264,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../../assets/style/container.scss'
+@import '../../../assets/style/container'
 </style>
--- src/plugins/LinCmsUi/views/data/badge/Badge.vue
+++ src/plugins/LinCmsUi/views/data/badge/Badge.vue
@@ -178,7 +178,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../../assets/style/container.scss';
+@import '../../../assets/style/container.scss';
 .item {
   margin-right: 40px;
 }
