585ab7a3189703bfd0c17c2d582783739e7f274c
JohnStream <1021283712@qq.com>
2019-06-30 22:10:25 +0800

    feat(linCmsUi): 初始化进度条及时间选择器展示页面

--- src/plugins/LinCmsUi/stage-config.js
+++ src/plugins/LinCmsUi/stage-config.js
@@ -71,6 +71,16 @@ const LinCmsUiRouter = {
           right: null,
         },
         {
+          title: 'Progress 进度条',
+          type: 'view',
+          name: null,
+          route: '/lin-cms-ui/data/progress/',
+          filePath: 'plugins/LinCmsUi/views/data/progress/Progress.vue',
+          inNav: true,
+          icon: 'iconfont icon-jiemiansheji',
+          right: null,
+        },
+        {
           title: 'Badge 标记',
           type: 'view',
           name: null,
@@ -254,6 +264,16 @@ const LinCmsUiRouter = {
           inNav: true,
           icon: 'iconfont icon-jiemiansheji',
           right: null,
+        },
+        {
+          title: 'TimePicker 时间选择器',
+          type: 'view',
+          name: null,
+          route: '/lin-cms-ui/form/timePicker',
+          filePath: 'plugins/LinCmsUi/views/form/TimePicker.vue',
+          inNav: true,
+          icon: 'iconfont icon-jiemiansheji',
+
         },
       ],
     },
