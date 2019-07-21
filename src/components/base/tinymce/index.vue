360502c302f15574b8c8e984d5c30298f0790a41
GongJS <382663074@qq.com>
2019-07-21 11:59:17 +0800

    update:新增富文本部分内置插件

--- src/components/base/tinymce/index.vue
+++ src/components/base/tinymce/index.vue
@@ -3,22 +3,55 @@
     <editor id="tinymceEditor" :init="tinymceInit" v-model="content" :key="tinymceFlag"></editor>
   </div>
 </template>
-
 <script>
 // eslint-disable-next-line
 import tinymce from 'tinymce/tinymce'
 import Editor from '@tinymce/tinymce-vue'
 import 'tinymce/themes/silver/theme'
-import 'tinymce/plugins/textcolor'
 import 'tinymce/plugins/advlist'
-import 'tinymce/plugins/table'
+import 'tinymce/plugins/anchor'
+import 'tinymce/plugins/autolink'
+// import 'tinymce/plugins/autoresize'
+// import 'tinymce/plugins/autosave'
+// import 'tinymce/plugins/bbcode'
+import 'tinymce/plugins/charmap'
+import 'tinymce/plugins/code'
+import 'tinymce/plugins/codesample'
+// import 'tinymce/plugins/colorpicker'
+// import 'tinymce/plugins/contextmenu'
+import 'tinymce/plugins/directionality'
+// import 'tinymce/plugins/emoticons'
+import 'tinymce/plugins/fullpage'
+import 'tinymce/plugins/fullscreen'
+import 'tinymce/plugins/help'
+import 'tinymce/plugins/hr'
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/imagetools'
+// import 'tinymce/plugins/importcss'
+import 'tinymce/plugins/insertdatetime'
+// import 'tinymce/plugins/legacyoutput'
+import 'tinymce/plugins/link'
 import 'tinymce/plugins/lists'
+// import 'tinymce/plugins/media'
+import 'tinymce/plugins/nonbreaking'
+// import 'tinymce/plugins/noneditable'
+import 'tinymce/plugins/pagebreak'
 import 'tinymce/plugins/paste'
 import 'tinymce/plugins/preview'
-import 'tinymce/plugins/fullscreen'
-import 'tinymce/plugins/image'
-import 'tinymce/plugins/code'
-import 'tinymce/plugins/link'
+import 'tinymce/plugins/print'
+// import 'tinymce/plugins/quickbars'
+// import 'tinymce/plugins/save'
+import 'tinymce/plugins/searchreplace'
+// import 'tinymce/plugins/spellchecker'
+// import 'tinymce/plugins/tabfocus'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/template'
+import 'tinymce/plugins/textcolor'
+import 'tinymce/plugins/textpattern'
+import 'tinymce/plugins/toc'
+import 'tinymce/plugins/visualblocks'
+import 'tinymce/plugins/visualchars'
+import 'tinymce/plugins/wordcount'
 
 export default {
   name: 'TinymceEditor',
@@ -38,6 +71,11 @@ export default {
     showMenubar: {
       type: Boolean,
       default: true,
+    },
+    toolbar: {
+      type: String,
+      // eslint-disable-next-line
+      default: ' undo redo |formatselect | bold italic strikethrough forecolor backcolor formatpainter | link image | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat | preview fullscreen code',
     },
   },
   components: {
@@ -64,9 +102,9 @@ export default {
       statusbar: false, // 隐藏编辑器底部的状态栏
       paste_data_images: true, // 允许粘贴图像
       menubar: this.showMenubar, // 隐藏最上方menu
-      plugins: 'advlist table lists paste preview fullscreen image code link',
       // eslint-disable-next-line
-      toolbar:' undo redo |fontselect fontsizeselect forecolor backcolor bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | link image | h1 h2 h3 blockquote table numlist bullist | preview fullscreen code',
+      plugins: 'print fullpage searchreplace autolink directionality visualblocks visualchars template codesample charmap hr pagebreak nonbreaking anchor toc insertdatetime wordcount textpattern help advlist table lists paste preview fullscreen image imagetools code link',
+      toolbar: this.toolbar,
       // eslint-disable-next-line
       images_upload_handler: async function(blobInfo, success, failure) {
         // eslint-disable-next-line
