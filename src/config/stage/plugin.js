1c372d2f50c5ba3a4e908d57e1e28fa40c19b5c4
vanoneang <525650856@qq.com>
2020-04-06 16:20:26 +0800

    fix: 解决大小写不敏感问题

--- src/config/stage/plugin.js
+++ src/config/stage/plugin.js
@@ -1,10 +1,8 @@
 // 本文件是自动生成, 请勿修改
 import chart from '@/plugin/chart/stage-config'
 import custom from '@/plugin/custom/stage-config'
-import fixSla from '@/plugin/fix-sla/stage-config'
 import linCmsUi from '@/plugin/lin-cms-ui/stage-config'
-import userPermis from '@/plugin/user-permis/stage-config'
 
-const pluginsConfig = [chart, custom, fixSla, linCmsUi, userPermis]
+const pluginsConfig = [chart, custom, linCmsUi]
 
 export default pluginsConfig
