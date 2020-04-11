77aecace9b1b7013b056aa9d9a7adb0173d7baf3
gongjs <382663074@qq.com>
2020-04-11 13:59:45 +0800

    权限组修改由弹窗改成page

--- src/view/admin/group/group-list.vue
+++ src/view/admin/group/group-list.vue
@@ -6,6 +6,7 @@
       :tableData="tableData"
       :operate="operate"
       @handleEdit="handleEdit"
+      @goToGroupEditPage="goToGroupEditPage"
       @handleDelete="handleDelete"
       @row-click="rowClick"
       v-loading="loading"
@@ -18,39 +19,23 @@
       class="groupListInfoDialog"
     >
       <div style="margin-top:-25px;">
-        <el-tabs v-model="activeTab" @tab-click="handleClick">
-          <el-tab-pane label="修改信息" name="修改信息" style="margin-top:10px;">
-            <el-form
-              status-icon
-              v-if="dialogFormVisible"
-              ref="form"
-              label-width="120px"
-              :model="form"
-              label-position="labelPosition"
-              :rules="rules"
-              style="margin-left:-35px;margin-bottom:-35px;margin-top:15px;"
-            >
-              <el-form-item label="分组名称" prop="name">
-                <el-input size="medium" clearable v-model="form.name"></el-input>
-              </el-form-item>
-              <el-form-item label="分组描述" prop="info">
-                <el-input size="medium" clearable v-model="form.info"></el-input>
-              </el-form-item>
-            </el-form>
-          </el-tab-pane>
-          <el-tab-pane label="配置权限" name="配置权限" style="margin-top:10px;">
-            <group-permissions
-              v-if="dialogFormVisible"
-              :id="id"
-              ref="groupPermissions"
-              @updatePermissions="updatePermissions"
-              @getCacheAuthIds="getCacheAuthIds"
-              @updateAllPermissions="updateAllPermissions"
-              style="margin-right:-30px;margin-left:-25px;margin-bottom:-10px;"
-            >
-            </group-permissions>
-          </el-tab-pane>
-        </el-tabs>
+        <el-form
+          status-icon
+          v-if="dialogFormVisible"
+          ref="form"
+          label-width="120px"
+          :model="form"
+          label-position="labelPosition"
+          :rules="rules"
+          style="margin-left:-35px;margin-bottom:-35px;margin-top:15px;"
+        >
+          <el-form-item label="分组名称" prop="name">
+            <el-input size="medium" clearable v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="分组描述" prop="info">
+            <el-input size="medium" clearable v-model="form.info"></el-input>
+          </el-form-item>
+        </el-form>
       </div>
       <div slot="footer" class="dialog-footer" style="padding-left:5px;">
         <el-button type="primary" @click="confirmEdit">确 定</el-button>
@@ -61,15 +46,12 @@
 </template>
 
 <script>
-import Utils from '@/lin/util/util'
 import Admin from '@/lin/model/admin'
 import LinTable from '@/component/base/table/lin-table'
-import GroupPermissions from './group-permission'
 
 export default {
   components: {
     LinTable,
-    GroupPermissions,
   },
   inject: ['eventBus'],
   data() {
@@ -85,9 +67,6 @@ export default {
         name: '',
         info: '',
       },
-      allPermissions: {}, // 所有分组权限
-      permissions: [], // 拥有的分组权限
-      cachePermissions: [], // 缓存第一次打开弹窗的数据
       cacheForm: {
         // 缓存第一次的表单信息
         name: '',
@@ -116,50 +95,22 @@ export default {
     },
     async confirmEdit() {
       // 修改分组信息
-      if (this.activeTab === '修改信息') {
-        if (this.form.name === '') {
-          this.$message.warning('请将信息填写完整')
-          return
-        }
-        if (this.cacheForm.name !== this.form.name || this.cacheForm.info !== this.form.info) {
-          // eslint-disable-line
-          const res = await Admin.updateOneGroup(this.form.name, this.form.info, this.id)
-          if (res.code < window.MAX_SUCCESS_CODE) {
-            this.$message.success(`${res.message}`)
-            this.getAllGroups()
-          }
-        }
-      } else {
-        let addRes = 0
-        let delRes = 0
-        // 判断是否更改了分组权限
-        if (this.permissions.sort().toString() !== this.cachePermissions.sort().toString()) {
-          const deletePermissions = this.cachePermissions
-            .concat(this.permissions)
-            .filter(v => !this.permissions.includes(v))
-          const addPermissions = this.cachePermissions
-            .concat(this.permissions)
-            .filter(v => !this.cachePermissions.includes(v))
-
-          console.log(deletePermissions)
-          console.log(addPermissions)
-
-          if (addPermissions.length > 0) {
-            addRes = await Admin.dispatchPermissions(this.id, addPermissions)
-          }
-          if (deletePermissions.length > 0) {
-            delRes = await Admin.removePermissions(this.id, deletePermissions)
-          }
-          if (addRes.code < window.MAX_SUCCESS_CODE || delRes.code < window.MAX_SUCCESS_CODE) {
-            this.$message.success('权限修改成功')
-          }
+      if (this.form.name === '') {
+        this.$message.warning('请将信息填写完整')
+        return
+      }
+      if (this.cacheForm.name !== this.form.name || this.cacheForm.info !== this.form.info) {
+        // eslint-disable-line
+        const res = await Admin.updateOneGroup(this.form.name, this.form.info, this.id)
+        if (res.code < window.MAX_SUCCESS_CODE) {
+          this.$message.success(`${res.message}`)
+          this.getAllGroups()
         }
       }
       this.dialogFormVisible = false
     },
     resetForm(formName) {
       this.$refs[formName].resetFields()
-      this.$refs.groupPermissions.getGroupPermissions()
     },
     // 获取所拥有的权限并渲染  由子组件提供
     handleEdit(val) {
@@ -176,6 +127,18 @@ export default {
       this.form.info = selectedData.info
       this.cacheForm = { ...this.form }
       this.dialogFormVisible = true
+    },
+    goToGroupEditPage(val) {
+      let selectedData
+      // 单击 编辑按键
+      if (val.index >= 0) {
+        selectedData = val.row
+      } else {
+        // 单机 table row
+        selectedData = val
+      }
+      this.id = selectedData.id
+      this.$router.push({ path: '/admin/group/edit', query: { id: selectedData.id } })
     },
     handleDelete(val) {
       let res
@@ -210,30 +173,6 @@ export default {
     rowClick(row) {
       this.handleEdit(row)
     },
-    // 弹窗打开时，判断某一分类权限是否全部选中
-    initModuleCheck(moduleName) {
-      const currentModuleChildrenArr = Object.keys(this.allPermissions[moduleName])
-      const intersect = Utils.getIntersect(currentModuleChildrenArr, this.permissions)
-      if (intersect.length === currentModuleChildrenArr.length) {
-        this.permissions.push(moduleName)
-      }
-    },
-    // 弹窗打开时，记录缓存所拥有的全部权限
-    getCacheAuthIds(ids) {
-      this.cachePermissions = ids
-    },
-    // 获取拥有的权限
-    updatePermissions(permissions) {
-      this.permissions = permissions
-    },
-    // 权限id集合
-    getAllAuthIds(allAuthIds) {
-      this.allAuthIds = allAuthIds
-    },
-    // 获取所有权限
-    updateAllPermissions(allPermissions) {
-      this.allPermissions = allPermissions
-    },
     // 弹框 右上角 X
     handleClose(done) {
       done()
@@ -242,7 +181,7 @@ export default {
     handleClick(tab) {
       this.activeTab = tab.name
     },
-    // 监听分组是否成功
+    // 监听分添加组是否成功
     async addGroup(flag) {
       if (flag === true) {
         await this.getAllGroups()
@@ -251,12 +190,16 @@ export default {
   },
   async created() {
     await this.getAllGroups()
-    this.tableColumn = [{ prop: 'name', label: '名称' }, { prop: 'info', label: '信息' }] // 设置表头信息
+    this.tableColumn = [
+      { prop: 'name', label: '名称' },
+      { prop: 'info', label: '信息' },
+    ] // 设置表头信息
     this.operate = [
-      { name: '编辑', func: 'handleEdit', type: 'primary' },
+      { name: '信息', func: 'handleEdit', type: 'primary' },
+      { name: '权限', func: 'goToGroupEditPage', type: 'info' },
       { name: '删除', func: 'handleDelete', type: 'danger' },
     ]
-    // 监听分组是否成功
+    // 监听添加分组是否成功
     this.eventBus.$on('addGroup', this.addGroup)
   },
   beforeDestroy() {
