26014d22d2572025a3886cdcd27e5a83f2aef869
Evan Wang <525650856@qq.com>
2022-09-28 14:18:03 +0800

    feat(plugin): delete chart plugin (#568)

--- src/config/stage/plugin.js
+++ src/config/stage/plugin.js
@@ -1,8 +1,7 @@
 // 本文件是自动生成, 请勿修改
-import chart from '@/plugin/chart/stage-config'
 import custom from '@/plugin/custom/stage-config'
 import linCmsUi from '@/plugin/lin-cms-ui/stage-config'
 
-const pluginsConfig = [chart, custom, linCmsUi]
+const pluginsConfig = [custom, linCmsUi]
 
 export default pluginsConfig
